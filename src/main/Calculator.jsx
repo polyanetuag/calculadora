b6fda6e6784958a157287d48e459138876105690
Polyane Tuag <pollyportugues@hotmail.com>
2021-09-02 16:45:17 -0300

    adding logic of first digits

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -2,7 +2,18 @@ import React, { Component } from 'react'
 import './Calculator.css'
 import Button from '../components/Button'
 import Display from '../components/Display'
+
+//estado inicial da calculadora
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+}
 export default class Calculator extends Component {
+  state = {...initialState} //estado inicial da calculadora
+
   constructor(props) {
     super(props)
     this.clearMemory = this.clearMemory.bind(this)
@@ -12,7 +23,7 @@ export default class Calculator extends Component {
 
   //limpar operação
   clearMemory() {
-    console.log('limpar')
+    this.setState({...initialState})
   }
 
   //colocar as operações
@@ -22,13 +33,33 @@ export default class Calculator extends Component {
 
   //adicionar dígito
   addDigit(n) {
-    console.log(n)
+    // verificação para impedir a adição de dois dígitos '.'
+    if (n === '.' && this.state.displayValue.includes('.')) {
+      return
+    }
+
+    //limpar display
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({ displayValue, clearDisplay:false})
+
+    //para armazenar os valores no values:[0, 0]
+    if (n !== '.') {
+      const i = this.state.current // índice do valor
+      const newValue = parseFloat(displayValue) // converter para float
+      const values = [...this.state.values] // novo array
+      values[i] = newValue //adicionando novo valor
+      this.setState({ values}) // array com os novos valores
+    }
+
   }
 
   render() {
     return (
       <div className="calculator">
-        <Display value={100} />
+        <Display value={this.state.displayValue} />
         <Button label="AC" click={this.clearMemory} triple />
         <Button label="/" click={this.setOperation} operation/>
         <Button label="7" click={this.addDigit}/>
